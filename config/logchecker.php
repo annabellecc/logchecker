7fe716dcb9f947ccdf646389472568bb3fdec217
Anna <1225754998@qq.com>
2017-10-11 15:52:18 +0800

    commit

--- config/logchecker.php
+++ config/logchecker.php
@@ -6,12 +6,6 @@ return [
 
     //绝对路径
     'log_paths' => [
-        'Jobs' => "/Users/Anna/wwwroot/jobs/storage/logs/laravel-" . date('Y-m-d') . ".log",
-        'Pay'  => "/Users/Anna/wwwroot/pay.verystar.cn/storage/logs/laravel-" . date('Y-m-d') . ".log",
-        'SDK'  => "/Users/Anna/wwwroot/verypay-sdk/hk.log",
-        'Api'  => "/Users/Anna/wwwroot/payapi_online/logs/log-" . date('Y-m-d') . ".log",
-        'Mini' => "/Users/Anna/wwwroot/m.pay.verystar.cn/storage/logs/laravel-" . date('Y-m-d') . ".log",
-        'Qr'   => "/Users/Anna/wwwroot/qr.pay.verystar.cn/storage/logs/laravel-" . date('Y-m-d') . ".log",
-        'Pic'  => "Users/Anna/wwwroot/pic.52inlove.com/storage/logs/laravel-" . date('Y-m-d') . ".log",
+        'test' => "/logpath/laravel-" . date('Y-m-d') . ".log",
     ],
 ];
